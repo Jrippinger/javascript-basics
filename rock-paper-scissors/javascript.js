ebb0ba30d7cacbbbf192e94c81e08b6c2711f58a
jrippinger <jrippinger@outlook.com>
2023-06-12 21:00:23 -0700

    Added UI elements

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,6 +1,6 @@
 //Global Variables
-let playerScore
-let computerScore
+let playerScore = 0
+let computerScore = 0
 let input
 
 //Function provides a random choice for the computer using Math.random
@@ -25,66 +25,64 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     if(playerSelection == "Rock" && computerSelection == "Scissors"){
         playerScore++
-        console.log("You Win! Rock crushes Scissors")
+        div.textContent = "Score: " + playerScore + "/" + computerScore + " You Win! Rock crushes Scissors"
     }
     if(computerSelection == "Rock" && playerSelection == "Scissors"){
         computerScore++
-        console.log("You Lose! Rock crushes Scissors")
+        div.textContent = "Score: " + playerScore + "/" + computerScore + " You Lose! Rock crushes Scissors"
     }
 
     if(playerSelection == "Paper" && computerSelection == "Rock"){
         playerScore++
-        console.log("You Win! Paper covers Rock")
+        div.textContent = "Score: " + playerScore + "/" + computerScore + " You Win! Paper covers Rock"
     }
     if(computerSelection == "Paper" && playerSelection == "Rock"){
         computerScore++
-        console.log("You Lose! Paper covers Rock")
+        div.textContent = "Score: " + playerScore + "/" + computerScore + " You Lose! Paper covers Rock"
     }
-
+    
     if(playerSelection == "Scissors" && computerSelection == "Paper"){
         playerScore++
-        console.log("You Win! Scissors cuts Paper")
+        div.textContent = "Score: " + playerScore + "/" + computerScore + " You Win! Scissors cuts Paper"
     }
     if(computerSelection == "Scissors" && playerSelection == "Paper"){
         computerScore++
-        console.log("You Lose! Scissors cuts Paper")
+        div.textContent = "Score: " + playerScore + "/" + computerScore + " You Lose! Scissors cuts Paper"
     }
     if(playerSelection === computerSelection){
-        console.log("There was a Tie")
+        div.textContent = "Score: " + playerScore + "/" + computerScore + " There was a Tie"
+    }
+    if((playerScore == 5) && (computerScore < 5)){
+        div.textContent = "Score: " + playerScore + "/" + computerScore + " The Player wins"
+    }
+    if((computerScore == 5) && (playerScore < 5)){
+        div.textContent = "Score: " + playerScore + "/" + computerScore + " The Computer wins"
+    }
+    if((playerScore == 5) && (computerScore == 5)){
+        div.textContent = "Game is broken, this should not happen"
     }
 }
 
-//Helper function that converts word turns input into having a capital first and and all else into lowercase
-function correct(word){
-    word = word.toLowerCase()
-    let fl = word.slice(0,1)
-    word = fl.toUpperCase() + word.slice(1,word.length)
-    return word
-}
+const body = document.body
 
-//A simple game of Rock Paper Scissors. For 5 turns the player will be asked for their input. If answer is not valid, they will be asked to try again. Once rounds are complete Results and scores are given
-function game(){
-    playerScore = 0
-    computerScore = 0
+const rock = document.createElement('button')
+rock.textContent = "Rock"
+rock.addEventListener('click', function(){
+    playRound(playerSelection = this.textContent, computerSelection = getComputerChoice())
+})
 
-    for(i=0; i<5; i++){
-        input = prompt("What is your move?")
-        input = correct(input)
-        
-        while((input != "Rock") && (input != "Paper") && (input != "Scissors")){
-            input = prompt("Invalid answer. What is your move?")
-            input = correct(input)
-        }
-             
-        playRound(input, computerSelection = getComputerChoice() )
-    }
-    if(playerScore>computerScore){
-        return "You Win! Congrats: " + playerScore + "/" + computerScore
-    }
-    if(playerScore<computerScore){
-        return "You Lose! Better luck next time: " + playerScore + "/" + computerScore
-    }
-    else{
-        return "There was a Tie: " + playerScore + "/" + computerScore
-    }
-}
+const paper = document.createElement('button')
+paper.textContent = "Paper"
+paper.addEventListener('click', function(){
+    playRound(playerSelection = this.textContent, computerSelection = getComputerChoice())
+})
+
+const scissors = document.createElement('button')
+scissors.textContent = "Scissors"
+scissors.addEventListener('click', function(){
+    playRound(playerSelection = this.textContent, computerSelection = getComputerChoice())
+})
+const div = document.createElement('div')
+div.textContent = "Score: " + playerScore + "/" + computerScore
+
+body.append(rock, paper, scissors, div)
